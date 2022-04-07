9b054a479222f27885f7db68bd6ba0ab6a39cc69
chococake22 <fightest21@gmail.com>
2022-04-07 22:22:23 +0900

    JPA를 이용한 게시판 만들기

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,15 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    // api로 넘긴 파라미터를 받아오는 어노테이션
+    // return에서 객체 필드값으로 넘겨준다.
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+
 
 }
--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -31,7 +33,19 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) // HTTP Header의 status를 검증
                 .andExpect(content().string(hello));
 
+    }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
 
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))   // param은 String만 가능하다.
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))    // json 응답값을 필드별로 검증하는 메서드
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
