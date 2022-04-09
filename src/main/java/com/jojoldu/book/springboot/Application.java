aefcdb22833267b4d9d26d4c04f4e5eec1e7b1db
chococake22 <fightest21@gmail.com>
2022-04-10 00:15:05 +0900

    thymeleaf 활용하기

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,7 +3,9 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 기능 활성화
 @SpringBootApplication
 public class Application {
 
--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -2,7 +2,7 @@ package com.jojoldu.book.springboot;
 
 
 import com.jojoldu.book.springboot.web.HelloController;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
